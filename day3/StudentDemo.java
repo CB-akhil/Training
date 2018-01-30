3f463767355ae765ebca9d62d1e86bee681b5232
Akhil <akhil@chargebee.com>
2018-01-30 18:16:40 +0530

    StudentDemo
    simple one but Too lengthy code

--- day3/StudentDemo.java
+++ day3/StudentDemo.java
@@ -1,66 +1,85 @@
+import java.util.Scanner;
+class Subject{
+	String sub1,sub2,sub3;
+	int m1,m2,m3;
+	public void setSub1(String temp){
+		sub1=temp;
+	}
+	public String getSub1(){
+		return sub1;
+	}
+	public void setSub2(String temp){
+		sub2=temp;
+	}
+	public String getSub2(){
+		return sub2;
+	}
+	public void setSub3(String temp){
+		sub3=temp;
+	}
+	public String getSub3(){
+		return sub3;
+	}
+	public void setm1(int temp){
+		m1=temp;
+	}
+	public int getm1(){
+		return m1;
+	}
+	public void setm2(int temp){
+		m2=temp;
+	}
+	public int getm2(){
+		return m2;
+	}
+	public void setm3(int temp){
+		m3=temp;
+	}
+	public int getm3(){
+		return m3;
+	}
+}
 class Student{
 	int studentdId;
 	String studentName;
 	boolean gender;
-	Subject subjects;
-	void setId(int temp){
+	public Subject subjects;
+	public void setId(int temp){
 		studentdId=temp;
 	}
-	int getId(){
+	public int getId(){
 		return studentdId;
 	}
-	void setName(String temp){
+	public void setName(String temp){
 		studentName=temp;
 	}
-	int getName(){
+	public String getName(){
 		return studentName;
 	}
-	void setGender(boolean temp){
+	public void setGender(boolean temp){
 		gender=temp;
 	}
-	boolean getGender(){
-		return gender
+	public boolean getGender(){
+		return gender;
 	}
-	void setSubjects(Subject temp){
+	public void setSubjects(Subject temp){
 		subjects=temp;
 	}
-	Subject getSubject(){
+	/*Subject getSubject(){
 		return subjects;
-	}
-}
-class subjects{
-	String sub1,sub2,sub3;
-	int m1,m2,m3;
-	void setSub1(String temp){
-		sub1=temp;
-	}
-	String getSub1(){
-		return sub1;
-	}
-	void setSub2(String temp){
-		sub2=temp;
-	}
-	String getSub2(){
-		return sub2;
-	}
-	void setSub3(String temp){
-		sub3=temp;
-	}
-	String getSub3(){
-		return sub3;
-	}
+	}*/
 }
 class RestultGenerator{
-	static generateResult(Student input){
+	static void generateResult(Student input){
 		System.out.println(" Report Card  ");
-		System.out.println("subject 1: " + input.sub1);
-		System.out.println("marks"+m1);
-		System.out.println("subject 2: " + input.sub2);
-		System.out.println("marks"+m2);
-		System.out.println("subject 3: " + input.sub3);
-		System.out.println("marks"+m3);
-		System.out.println("Total :" + m1+m2+m3);
-		System.out.println("Average : " + (float)(m1+m2+m3)/3);
+		System.out.println("subject 1: " + input.subjects.getSub1());
+		System.out.println("marks"+input.subjects.getm1());
+		System.out.println("subject 2: " + input.subjects.getSub2());
+		System.out.println("marks"+input.subjects.getm2());
+		System.out.println("subject 3: " + input.subjects.getSub3());
+		System.out.println("marks"+input.subjects.getm3());
+		System.out.println("Total :" + input.subjects.getm1()+input.subjects.getm2()+input.subjects.getm3());
+		System.out.println("Average : " + (float)(input.subjects.getm1()+input.subjects.getm2()+input.subjects.getm3())/3);
 	}
 }
 public class StudentDemo{
@@ -70,15 +89,36 @@ public class StudentDemo{
 	}
 	public static void main(String[] args) {
 		String input;
-		Scanner scanner=new Scanner();
+		Scanner scanner=new Scanner(System.in);
 		Student student=new Student();
+		System.out.println("Enter Sudent Id:");
+		int num=scanner.nextInt();
+		student.setId(num);
 		System.out.println("Enter Sudent name:");
 		input=scanner.next();
 		student.setName(input);
 		boolean bool;
 		System.out.println("enter Student gender ");
 		bool=scanner.nextBoolean();
-
-
+		Subject subject=new Subject();
+		System.out.println("Enter Subject1 name:");
+		input=scanner.next();
+		subject.setSub1(input);
+		System.out.println("Enter Subject1 marks:");
+		num=scanner.nextInt();
+		subject.setm1(num);
+		System.out.println("Enter Subject2 name:");
+		input=scanner.next();
+		subject.setSub2(input);
+		System.out.println("Enter Subject2 marks:");
+		num=scanner.nextInt();
+		subject.setm2(num);
+		System.out.println("Enter Subject3 name:");
+		input=scanner.next();
+		subject.setSub3(input);
+		System.out.println("Enter Subject3 marks:");
+		num=scanner.nextInt();
+		subject.setm3(num);
+		student.setSubjects(subject);
 	}
 }
