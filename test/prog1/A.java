df4d6fc8345be59167faff4b377fd0d105c90ea8
Akhil <akhil@chargebee.com>
2018-01-31 17:47:45 +0530

    test
    package and import java
    calling a funciton present in class A by main methon in class B

---
+++ test/prog1/A.java
@@ -0,0 +1,10 @@
+package prog1;
+public class A{
+	int a;
+	public void print(){
+		System.out.println("print from A");
+	}
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ test/prog2/B.java
@@ -0,0 +1,7 @@
+import prog1.A;
+class B {
+	public static void main(String[] args) {
+			A a = new A();
+			a.print();
+		} 
+}
