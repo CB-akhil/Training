99e33df3f1dba136a55013c1b62374b29fdf44fe
Akhil <akhil@chargebee.com>
2018-02-06 15:08:18 +0530

    week2
    exercise 1

--- week2/day1/MapExample.java
+++ week2/day1/MapExample.java
@@ -1,6 +1,25 @@
 import java.util.*;  
 class MapExample{
 	public static void main(String[] args) {
-		Map<int,ArrayList<String>> table= new table<>();
+		String[] array = {"length","of","the","word","list","of","with","that"};
+		Map<Integer,List<String>> table= new HashMap<>();
+		for(String temp:array){
+			if(table.containsKey(temp.length())){
+				List<String> l = table.get(temp.length());
+				l.add(temp);
+				table.put(temp.length(),l);
+			}else{
+				List<String> l = new ArrayList<>();
+				l.add(temp);
+				table.put(temp.length(),l);
+			}
+		}
+		for (Map.Entry<Integer,List<String>> entry : table.entrySet()) {
+		    System.out.print("Key = " + entry.getKey() + ", Value = " );
+		    List<String> l =entry.getValue();
+		    for(String a:l)
+		    	System.out.print(a + " ");
+		    System.out.println();
+		}
 	}
 }
