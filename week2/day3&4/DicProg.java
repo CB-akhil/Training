7feef6a7423f7aaa04b1a2d2b07d0011a9c12639
Akhil <akhil@chargebee.com>
2018-02-07 12:50:39 +0530

    dicrectory program
    Write a program to scan a directory and its sub directories and print all the file extensions along with the number of files with that extension.

--- week2/day3&4/DicProg.java
+++ week2/day3&4/DicProg.java
@@ -1,30 +1,58 @@
 import java.io.*;
+import java.util.*;
 import java.util.Scanner;
 class DicProg{
+	static Map<String,Integer>  table = new HashMap<>();
 
-	public static void addFilesAndSubDirectoriesfiles(String directoryName, List list){
-        File directory = new File(directoryName);
-        //get all the files from a directory
-        File[] fList = directory.listFiles();
-        for (File file : fList){
-            if (file.isFile()){
-                System.out.println(file.getAbsolutePath());
-            } else if (file.isDirectory()){
-                listFilesAndFilesSubDirectories(file.getAbsolutePath());
-            }
-        }
-    }
+	static String getExtension(String filename){
+		int startIndex=filename.lastIndexOf('.');
+		return filename.substring(startIndex,filename.length());
+	}
+
+	static void add(String extension){
+		if(table.containsKey(extension)){
+			int val=table.get(extension);
+			val++;
+			table.put(extension,val);
+		}else{
+			table.put(extension,1);
+		}
+	}
+
+	public static void scanAndAdd(File dir) {
+		try{
+			File[] files = dir.listFiles();
+			for (File file : files) {
+				if (file.isDirectory()) {
+					//System.out.println("directory:" + file.getCanonicalPath());
+					scanAndAdd(file);
+				} else {
+					DicProg.add(getExtension(file.getName()));
+				}
+			}	
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static void printTable(){
+		Iterator it = table.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry)it.next();
+			System.out.println(pair.getKey() + " = " + pair.getValue());
+			it.remove(); // avoids a ConcurrentModificationException
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new  Scanner(System.in);
 		String input;
-		System.out.println("Enter the Directory to Search:");
+		System.out.println("Enter the path of directory");
 		input=scanner.next();
 		File directory = new File(input);
-		File[] flist=directory.listfiles();
-		List<File> list = new ArrayList<>();
-		for(File file:flist){
-
-		}
+		scanner.close();
+		DicProg.scanAndAdd(directory);
+		DicProg.printTable();
 	}
 }
